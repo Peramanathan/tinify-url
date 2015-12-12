ab4c9b87f9e161e6a607d535f2dc78a16ddf59e7
Peramanathan <premsakthis2@gmail.com>
2015-12-12 20:26:32 +0100

    More test cases

--- tinify_tests.py
+++ tinify_tests.py
@@ -19,18 +19,31 @@ class TinifyTestCase(unittest.TestCase):
                              ['need', 'not assigned', datetime.now()])
             tinify.update_db(tinify.ASSIGN_KEYVALS_QUERY,
                              ['we', 'not assigned', datetime.now()])
+            tinify.update_db(tinify.ASSIGN_KEYVALS_QUERY,
+                             ['test', 'http:testing.com', datetime.now()])
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(tinify.app.config['DATABASE'])
 
     def feed_sample_url(self):
-
         long_url = "techcrunch.com/2015/11/10/gmail-we-need-to-talk"
-
         return self.app.post('/', data=dict(longUrl=long_url),
                              follow_redirects=True)
 
+    def get_key_for_url(self, long_url):
+        return self.app.post('/', data=dict(longUrl=long_url),
+                             follow_redirects=True)
+
+    def goto_path(self, word):
+        return self.app.get('/' + word)
+
+    # Unit & Integration test 1: is index correctly loaded? code 200
+    def test_index(self):
+        rv = self.app.get('/')
+        assert 'paste your long url' in rv.data
+
+    # Unit & Integration test 2: does long url feed result in short url?
     def test_feed_url(self):
         rv = self.feed_sample_url()
         href = "techcrunch.com/2015/11/10/gmail-we-need-to-talk"
@@ -38,9 +51,13 @@ class TinifyTestCase(unittest.TestCase):
         assert href in rv.data
         assert shorturl in rv.data
 
-    def test_index(self):
-        rv = self.app.get('/')
-        assert 'paste your long url' in rv.data
+    # Unit & Integration test 3: redirection works?
+    def test_redirection(self):
+        rv = self.goto_path('test')
+        info = 'You should be redirected automatically to target URL: '
+        redirection_url = '<a href="http:testing.com">http:testing.com</a>'
+        success = info + redirection_url
+        assert success in rv.data
 
 
 if __name__ == '__main__':
