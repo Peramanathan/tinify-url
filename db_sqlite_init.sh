3c15a547cb15b12612b2b9210da10546f98e8873
Peramanathan <premsakthis2@gmail.com>
2015-12-12 18:10:19 +0100

    Latest shell script for populating db

--- db_sqlite_init.sh
+++ db_sqlite_init.sh
@@ -6,13 +6,13 @@ echo "Date is $DATE"
 UP=`date ; uptime`
 echo "Uptime is $UP"
 
-sqlite3 wordlist.db  "CREATE TABLE wordlist(k BLOB PRIMARY KEY, v BLOB);"
+sqlite3 wordlist.db  "CREATE TABLE wordlist(k TEXT PRIMARY KEY, v TEXT, up INTEGER);"
 echo "Inserting ..."
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	word="$(echo "$line" | tr -cd '[[:alnum:]]')"
 	word_lower=`echo "$word" | sed 's/./\L&/g'`
-	sqlite3 wordlist.db  "insert into wordlist (k,v) values ('$word_lower','not assigned');"
+	sqlite3 wordlist.db  "insert into wordlist (k,v,up) values ('$word_lower','not assigned','$DATE');"
 done < "$1"
 
 echo "Insertion finished!"
